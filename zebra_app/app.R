68ea22fd6ae040400b2fafd4afa471bab0c60bee
layla-zai <laylaahmadzai06@gmail.com>
2025-07-22 09:57:45 -0400

    Update app.R
    Requested changes complete.

--- zebra_app/app.R
+++ zebra_app/app.R
@@ -10,29 +10,29 @@
 
 library(shiny)
 library(bslib)
+library(vroom)
+library(dplyr)
+library(ggplot2)
+library(stringr)
+library(writexl)
+library(DT)
+library(plotly)
+library(tidyr)
 
 ui <- page_sidebar(
-  title = "ZebraBox Data Upload",
+  title = "ZebraBox Activity Analysis",
   sidebar = sidebar(
-    # Upload Data file input
     fileInput("data_file", "Upload Data:"),
-    
-    # Upload Metadata file input + Download template button
     fileInput("metadata_file", "Upload Metadata:"),
-    downloadButton("download_template", "Download template"),
-    
-    # Optional text input for metadata order
+    downloadButton("download_template", "Download metadata template", class = "btn btn-secondary btn-md"),
     textInput("metadata_order", "(optional) Specify metadata order:"),
-    
-    # Run button
-    actionButton("run", "Run")
+    actionButton("run", "Run", class = "btn btn-primary btn-lg")
   ),
   
   card(
-    
     conditionalPanel(
       condition = "input.run == 0",
-      tags$h3("Please upload your data and click Run to begin.")
+      tags$h5("Please upload your data and click Run to begin.")
     ),
     
     conditionalPanel(
@@ -48,10 +48,16 @@ ui <- page_sidebar(
         column(
           width = 9,
           navset_card_underline(
-            nav_panel("Plot 1", plotOutput("plot1")),
-            nav_panel("Plot 2", plotOutput("plot2")),
-            nav_panel("Plot 3", plotOutput("plot3")),
-            nav_panel("Plot 4", plotOutput("plot4"))
+            nav_panel("All Fish by Minute", plotOutput("plot1")),
+            nav_panel("Condition Summary", plotOutput("plot2")),
+            nav_panel("Plate Map / Well Activity", plotOutput("plot3")),
+            nav_panel("Mean Activity", plotOutput("plot4")),
+            nav_panel("Percent Change", plotOutput("plot5")),
+            nav_panel("Activity by Period", plotOutput("plot6")),
+            nav_panel("Percent Change by Period", plotOutput("plot7")),
+            nav_panel("Statistical Summary", DT::dataTableOutput("stats_table")),
+            nav_panel("Plate Map", plotlyOutput("plate_map")),
+            nav_panel("Cleaned Data", DT::dataTableOutput("cleaned_table"))
           )
         )
       )
@@ -59,23 +65,80 @@ ui <- page_sidebar(
   ),
   
   theme = bs_theme(
-    bg = "white",
+    bg = "#FAFAF8",
     fg = "black",
-    primary = "#E69F00",
-    secondary = "#0072B2",
-    success = "#009E73",
-    base_font = font_google("Inter")
+    primary = "#429E99",
+    secondary = "#B2DFDB",
+    success = "#00796B",
+    base_font = font_google("Inter"),
+    bootswatch = "flatly"
   )
 )
-  
+
 server <- function(input, output, session) {
   
+  metadata <- reactive({
+    req(input$metadata_file)
+    readxl::read_excel(input$metadata_file$datapath)
+  })
+  
+  zebrabox_data <- reactive({
+    req(input$data_file)
+    raw_data <- read.delim(input$data_file$datapath, sep = "\t", header = TRUE)
+    
+    raw_data %>% 
+      mutate(time_min = start / 60, 
+             # experiment_time_of_day = '',
+             period2 = ifelse(nchar(time_min) == 1, 1,
+                              as.integer(str_extract(time_min, '^[0-9]')) + 1),
+             plate = str_extract(location, 'Loc[AB]'),
+             light = ifelse(period2 %% 2 != 0 & period2 != 1, 'dark', 'light')) %>%
+      separate(aname, into = c('well', 'name'), sep = '_') %>%
+      left_join(metadata(), by = join_by(well, plate == box_used)) %>%
+      select(plate, treatment, well, time_min, light, period, 
+             datatype, activity = actinteg, everything()) %>%
+      filter(!is.na(treatment), timebinid == 1) 
+  })
+  
+  output$cleaned_table <- DT::renderDataTable({
+    req(zebrabox_data())
+    DT::datatable(zebrabox_data(),
+                  extensions = 'Buttons',
+                  options = list(dom = 'Blfrtip',
+                                 buttons = c('copy', 'csv', 'excel'),
+                                 lengthMenu = list(c(10, 25, 50, -1),
+                                                   c(10, 25, 50, "All"))),
+                  caption = htmltools::tags$caption(
+                    style = 'caption-side: top; text-align: left; color:black; font-size:200%;',
+                    'Zebrabox'))
+  })
+  
   output$download_template <- downloadHandler(
-    filename = function() "metadata_template.csv",
-    content = function(file) {
-      write.csv(data.frame(SampleID = "", Condition = ""), file, row.names = FALSE)
+    filename = function() { 
+      "0000-00-00_zebrabox_experiment_template.xlsx"
+    },
+    content = function(file) {file.copy("C:/Users/layla/OneDrive/Documents/GitHub/zebrabox/0000-00-00_zebrabox_experiment_template.xlsx", file)
     }
   )
+  
+  output$plate_map <- plotly::renderPlotly({
+    req(zebrabox_data())
+    
+    plate_map <- zebrabox_data() %>%
+      separate(well, into = c('well_row', 'well_column'), sep = 1) %>%
+      filter(datatype == 'QuantizationSum', period2 == 3) %>%
+      ggplot(aes(x = well_column, y = well_row, text = treatment,
+                 color = activity)) +
+      geom_point(size = 4) +
+      scale_color_continuous(limits = c(0, 7000)) +
+      scale_x_discrete(position = "top")  +
+      scale_y_discrete(limits = rev) +
+      facet_wrap(~ plate, ncol = 2) +
+      labs(x = NULL, y = NULL, color = 'Activity\n1st Dark\nPeriod') +
+      theme_minimal()
+    
+    plotly::ggplotly(plate_map)
+  })
   
   output$download_report <- downloadHandler(
     filename = function() "zebrabox_report.txt",
@@ -87,16 +150,35 @@ server <- function(input, output, session) {
   output$download_data <- downloadHandler(
     filename = function() "processed_data.tsv",
     content = function(file) {
-      # For now, just write empty placeholder data
       write.table(data.frame(), file, sep = "\t", row.names = FALSE, quote = FALSE)
     }
   )
   
-  # Placeholder empty plots just to make UI look right
-  output$plot1 <- renderPlot({ plot.new() })
+  output$plot1 <- renderPlot({
+    data <- zebrabox_data()
+    data %>%
+      filter(datatype == 'QuantizationSum') %>%
+      ggplot(aes(x = plate, y = activity)) +
+      geom_boxplot(alpha = 0.5, fill = "orange") +
+      geom_jitter(width = 0.2, alpha = 0.6) +
+      labs(
+        title = "Activity by Plate (LocA/LocB)",
+        x = "Plate Location",
+        y = "Activity (QuantizationSum)"
+      ) +
+      theme_classic(base_size = 14)
+  })
+  
   output$plot2 <- renderPlot({ plot.new() })
   output$plot3 <- renderPlot({ plot.new() })
   output$plot4 <- renderPlot({ plot.new() })
+  output$plot5 <- renderPlot({ plot.new() })
+  output$plot6 <- renderPlot({ plot.new() })
+  output$plot7 <- renderPlot({ plot.new() })
+  
+  output$stats_table <- DT::renderDataTable({
+    data.frame(Message = "Statistics will appear here")
+  })
   
 }
 
